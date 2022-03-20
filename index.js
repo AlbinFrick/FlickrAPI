8e69b0c99a6b4d78d3c1ba98ce4ccd470b3f38b8
Albin Frick <abbzez@gmail.com>
2022-03-20 23:01:15 +0100

    adds a 404 state

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
-import express from 'express';
-import fetch from 'node-fetch';
-import cors from 'cors';
-import dotenv from 'dotenv'
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+import dotenv from "dotenv";
 dotenv.config();
-
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.listen(
-	PORT,
-	() => console.log(`it's alive on http://localhost:${PORT}`)
-)
+app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`));
 
 /**
  * This endpoint will fetch imageURIs with the given tag (category),
  * how many images of the category, and what size the images should be.
- * 
+ *
  * ? Sizes can be found here: https://www.flickr.com/services/api/misc.urls.html
- * 
+ *
  * *params: req, res
  */
-app.get('/search-image/:tag/:amount/:page', async (req, res) => {
-	const { tag, amount, page } = req.params;
-	var url = `https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${process.env.FLICKR_API_KEY}&tags=${tag}&per_page=${amount}&page=${page}&format=json&nojsoncallback=1`
-	fetch(url)
-		.then((response) => {
-			return response.json()
-		})
-		.then((jsonRes) => {
-			let imgArray = jsonRes.photos.photo.map((img) => {
-				var imgURI = `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}`
-				return {
-					title: img.title,
-					imgURI: imgURI
-				}
-			})
-			res.status(200).send(imgArray)
-		}).catch((error) => {
-			console.log(error)
-			res.status(400).send(error)
-		})
-})
+app.get("/search-image/:tag/:amount/:page", async (req, res) => {
+  const { tag, amount, page } = req.params;
+  var url = `https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${process.env.FLICKR_API_KEY}&tags=${tag}&per_page=${amount}&page=${page}&format=json&nojsoncallback=1`;
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonRes) => {
+      let imgArray = jsonRes.photos.photo.map((img) => {
+        var imgURI = `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}`;
+        return {
+          title: img.title,
+          imgURI: imgURI,
+        };
+      });
+      if (imgArray.length == 0)
+        res.status(404).send('Inga bilder hittades')
+      else
+        res.status(200).send(imgArray);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
+
